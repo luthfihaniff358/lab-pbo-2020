124b0d4603a4de46d9320d1ee6cfca4010a56b4b
luthfihanif358 <luthfihanif358@gmail.com>
2020-10-15 16:30:51 +0700

    Membuat Code Pertemuan 2

---
+++ Pertemuan 2/prak3Lat5.java
@@ -0,0 +1,10 @@
+public class prak3Lat5 {
+public static void main(String args[]) {
+System.out.println("Tanggal : " + args[0]);
+System.out.println("Bulan : " + args[1]);
+System.out.println("Tahun : " + args[2]);
+}
+}
+/*
+Program diatas untuk mendapatkan nilai argumen pada method main
+*/
