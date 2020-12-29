1ba92e54e3aa41d29a80fe846409d639c93432f3
luthfihanif358 <luthfihanif358@gmail.com>
2020-12-29 23:49:01 +0700

    Membuat Code Pertemuan 6 7 8 9 10

--- Pertemuan 5/Manajer.java
+++ Pertemuan 5/Manajer.java
@@ -2,7 +2,7 @@ public class Manajer extends Pegawai {
 private String departemen;
 public Manajer(String nama, double gaji, String departemen)
 {
-Pegawai p = new Pegawai(nama, gaji);
+super(nama, gaji);
 this.departemen = departemen;
 }
 public void setDepartemen(String departemen)
--- Pertemuan 5/Parent.java
+++ Pertemuan 5/Parent.java
@@ -1,9 +1,2 @@
 public class Parent {
 }
-public class Child extends Parent {
-int x;
-public Child() {
-x = 5;
-super();
-}
-}
